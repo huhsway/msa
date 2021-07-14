dd5a7165d10650f9301b3606b6d1ef539dc36bcf
huhsway <seunghoe9542@naver.com>
2021-07-15 00:00:56 +0900

    bookCatalog의 yml, json 파일 업데이트, 도서 서비스에 book 엔티티를 생성한다.

---
+++ book/src/main/java/com/my/book/service/dto/BookDTO.java
@@ -0,0 +1,78 @@
+package com.my.book.service.dto;
+
+import java.io.Serializable;
+
+/**
+ * A DTO for the {@link com.my.book.domain.Book} entity.
+ */
+public class BookDTO implements Serializable {
+    
+    private Long id;
+
+    private String title;
+
+    private String author;
+
+    private String description;
+
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof BookDTO)) {
+            return false;
+        }
+
+        return id != null && id.equals(((BookDTO) o).id);
+    }
+
+    @Override
+    public int hashCode() {
+        return 31;
+    }
+
+    // prettier-ignore
+    @Override
+    public String toString() {
+        return "BookDTO{" +
+            "id=" + getId() +
+            ", title='" + getTitle() + "'" +
+            ", author='" + getAuthor() + "'" +
+            ", description='" + getDescription() + "'" +
+            "}";
+    }
+}
