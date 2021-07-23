055d2e21d864862223845a25a46f6c18aed535fa
huhsway <seunghoe9542@naver.com>
2021-07-23 17:41:15 +0900

    내부 영역 - 리포지토리 개발

--- rental/src/main/java/com/my/rental/repository/RentalRepository.java
+++ rental/src/main/java/com/my/rental/repository/RentalRepository.java
@@ -9,6 +9,9 @@ import org.springframework.stereotype.Repository;
  * Spring Data  repository for the Rental entity.
  */
 @SuppressWarnings("unused")
+//Rental 엔티티의 리포지토리 인터페이스
 @Repository
 public interface RentalRepository extends JpaRepository<Rental, Long> {
+
+    Optional<Rental> findByUserId(Long userId);
 }
